18376a6f673d8ec21a2b3f88f02eceb62bd548e3
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-08-03 20:48:56 +0530

    common: Delete utils.mk and add some moar flags
    * Coz now we have it in vendor/wave after https://github.com/Wave-Project/vendor_wave/commit/6de8965436b7c7e49d239ffd386c9373cc939e4d

--- common.mk
+++ common.mk
@@ -1,14 +1,18 @@
+#
+# Common stuff - packages, flags and configs for QC devices
+#
+
+# Default board flags for QC devices
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_HAS_QC_KERNEL_SOURCE := true
-
-# Allow building audio encoders
-TARGET_USES_QCOM_MM_AUDIO := true
-
 BOARD_USES_ADRENO := true
 BOARD_USES_QCNE := true
 
-# include additional build utilities
--include device/qcom/common/utils.mk
+# Enable media extensions
+TARGET_USES_MEDIA_EXTENSIONS := true
+
+# Allow building audio encoders
+TARGET_USES_QCOM_MM_AUDIO := true
 
 # SECCOMP Extension
 BOARD_SECCOMP_POLICY += device/qcom/common/seccomp
@@ -24,4 +28,9 @@ PRODUCT_BOOT_JARS += tcmclient
 PRODUCT_BOOT_JARS += com.qti.dpmframework
 PRODUCT_BOOT_JARS += dpmapi
 PRODUCT_BOOT_JARS += com.qti.location.sdk
+endif
+
+# QCOM HW crypto
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
 endif
